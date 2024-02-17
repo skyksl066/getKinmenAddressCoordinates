608745c11000cf56e3645cdb46c64a6e14d30d9d
AlexKuo <skyksl066@gmail.com>
2024-02-17 23:50:02 +0800

    add mark

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from pyproj import Proj, Transformer
 from bs4 import BeautifulSoup
 import os
 import threading
+import json
 
 # 失敗重試次數
 MAX_RETRIES = 2
@@ -21,12 +22,6 @@ MAX_RETRIES = 2
 API = 'https://urban.kinmen.gov.tw/kmgis/map/AcHandler.ashx'
 # LOG檔命名規則
 LOG_PATH = f'Logs/{datetime.today().strftime("%Y%m%d")}.log'
-# 保存文件名稱
-CSV_FILE = 'data.csv'
-# 要寫入的欄位名稱
-FIELDNAMES = ['FULL_ADDR', 'LATITUDE', 'LONGITUDE']
-# 保存處理位置的文件名
-RESUME_FILE = 'resume.txt'
 # 每次處理兩個鄉鎮
 BATCH_SIZE = 2
 
@@ -83,16 +78,49 @@ def searchRoads(txt='', retry_count=0):
     return check_status(response, 'ROAD', retry_count, lambda count: searchRoads(txt, count))
 
 
+def getMarkMainClassList(retry_count=0):
+    loguru.logger.info('取得主分類')
+    response = requests.post(API, data={'CMD': 'GETMARKMAINCLASSLIST'})
+    return check_status(response, ['Type', 'TypeId'], retry_count, lambda count: getMarkMainClassList(count))
+
+
+def getMarkSubClassList(main, retry_count=0):
+    loguru.logger.info(f'取得次分類 主分類:{main}')
+    response = requests.post(API, data={'CMD': 'GETMARKSUBCLASSLIST', 'VAL': main})
+    return check_status(response, ['SubType', 'SubTypeId'], retry_count, lambda count: getMarkSubClassList(main, count))
+
+
+def getMarkList(main, sub, retry_count=0):
+    loguru.logger.info(f'取得地標 主分類:{main} 次分類:{sub}')
+    response = requests.post(API, data={'CMD': 'GETMARKLIST', 'VAL': main, 'CODE': sub})
+    try:
+        # 使用.json()方法解析JSON數據
+        json_data = response.json()
+    except Exception as e:
+        loguru.logger.debug(f'Error decoding JSON: {e}.')
+        loguru.logger.debug(f'Response text: {response.text}')
+        if retry_count < MAX_RETRIES:
+            loguru.logger.info('10秒後再試一次')
+            time.sleep(10)
+            return getMarkList(main, sub, retry_count + 1)
+        else:
+            loguru.logger.info('已達最大重試次數，放棄嘗試')
+            loguru.logger.error(f'mainType: {main} subType: {sub}')
+            return None
+    return json_data if json_data else None
+
+
 def getSubLmCode(type, retry_count=0):
     # type 1~15
     loguru.logger.info(f'取得小類 大類：{type}')
     response = requests.post(API, data={'CMD': 'GETSUBLMCODE', 'VAL': type})
     return check_status(response, 'SubType', retry_count, lambda count: getSubLmCode(type, count))
-    
+
 
 def getLandMarkList(type, subtype, retry_count=0):
     loguru.logger.info(f'取得地標 大類：{type} 小類:{subtype}')
-    response = requests.post(API, data={'CMD': 'GETLANDMARKLIST', 'VAL': type, 'CODE': subtype})
+    response = requests.post(
+        API, data={'CMD': 'GETLANDMARKLIST', 'VAL': type, 'CODE': subtype})
     try:
         # 使用.json()方法解析JSON數據
         json_data = response.json()
@@ -156,7 +184,8 @@ def getLanes(town, road, retry_count=0):
     loguru.logger.info(f'取得巷 鄉鎮：{town} 門牌：{road}')
     # 定義POST請求的URL和payload（要發送的數據）
     # 發送POST請求
-    response = session.post(API, data={'CMD': 'GETADDRLANE', 'VAL': town, 'CODE': road})
+    response = session.post(
+        API, data={'CMD': 'GETADDRLANE', 'VAL': town, 'CODE': road})
     return check_status(response, 'LANE', retry_count, lambda count: getLanes(town, road, count))
 
 
@@ -184,7 +213,8 @@ def getAlleys(town, road, lane, retry_count=0):
     loguru.logger.info(f'取得弄 鄉鎮：{town} 門牌：{road} 巷：{lane}')
     # 定義POST請求的URL和payload（要發送的數據）
     # 發送POST請求
-    response = session.post(API, data={'CMD': 'GETADDRALLEY', 'VAL': town, 'CODE': road, 'OTHER': lane})
+    response = session.post(
+        API, data={'CMD': 'GETADDRALLEY', 'VAL': town, 'CODE': road, 'OTHER': lane})
     return check_status(response, 'ALLEY', retry_count, lambda count: getAlleys(town, road, lane, count))
 
 
@@ -214,7 +244,8 @@ def getDoors(town, road, lane, alley, retry_count=0):
     loguru.logger.info(f'取得號 鄉鎮：{town} 門牌：{road} 巷：{lane} 弄：{alley}')
     # 定義POST請求的URL和payload（要發送的數據）
     # 發送POST請求
-    response = session.post(API, data={'CMD': 'GETADDRDOOR', 'VAL': town, 'CODE': road, 'OTHER': lane, 'MORE': alley})
+    response = session.post(API, data={
+                            'CMD': 'GETADDRDOOR', 'VAL': town, 'CODE': road, 'OTHER': lane, 'MORE': alley})
     return check_status(response, 'NUMBER', retry_count, lambda count: getDoors(town, road, lane, alley, count))
 
 
@@ -246,7 +277,8 @@ def getXY(town, road, lane, alley, door, retry_count=0):
     loguru.logger.info(f'取得座標 鄉鎮：{town} 門牌：{road} 巷：{lane} 弄：{alley} 號：{door}')
     # 定義POST請求的URL和payload（要發送的數據）
     # 發送POST請求
-    response = session.post(API, data={'CMD': 'GETXY', 'VAL': town, 'CODE': road, 'OTHER': lane, 'MORE': alley, 'THEN': door})
+    response = session.post(API, data={
+                            'CMD': 'GETXY', 'VAL': town, 'CODE': road, 'OTHER': lane, 'MORE': alley, 'THEN': door})
     try:
         # 使用.json()方法解析JSON數據
         json_data = response.json()
@@ -259,7 +291,8 @@ def getXY(town, road, lane, alley, door, retry_count=0):
             return getXY(town, road, lane, alley, door, retry_count + 1)
         else:
             loguru.logger.info('已達最大重試次數，放棄嘗試')
-            loguru.logger.error(f'town: {town} road: {road} lane: {lane} alley: {alley} door: {door}')
+            loguru.logger.error(
+                f'town: {town} road: {road} lane: {lane} alley: {alley} door: {door}')
             return None
     return json_data[0] if json_data else None
 
@@ -272,7 +305,7 @@ def check_status(response, key, retry_count, callback):
     ----------
     response : requests.Response
         HTTP響應對象.
-    key : str
+    key : str or list
         JSON數據中要提取的鍵.
     retry_count : int
         失敗重試次數.
@@ -288,9 +321,16 @@ def check_status(response, key, retry_count, callback):
     try:
         # 使用.json()方法解析JSON數據
         json_data = response.json()
-        arr = [dist[key] for dist in json_data]
-        loguru.logger.debug(arr)
-        return arr
+        if type(key) == list:
+            obj = {}
+            for d in json_data:
+                obj.update({d[key[1]]: d[key[0]]})
+            loguru.logger.debug(obj)
+            return obj
+        else:
+            arr = [obj[key] for obj in json_data]
+            loguru.logger.debug(arr)
+            return arr
     except Exception as e:
         loguru.logger.debug(f'Error decoding JSON: {e}.')
         loguru.logger.debug(f'Response text: {response.text}')
@@ -301,53 +341,73 @@ def check_status(response, key, retry_count, callback):
             return callback(retry_count + 1)
         else:
             loguru.logger.info('已達最大重試次數，放棄嘗試')
-            return ['']
+            return {} if type(key) == list else []
 
 
-def read_resume():
+def read_data(file_name, start, end):
     """
-    從 resume.csv 文件中讀取所有處理的位置
-
-    Returns
-    -------
-    result : set
-        包含已處理位置的集合.
-
-    """
-    resume = set()
-    if os.path.exists(RESUME_FILE):
-        with open(RESUME_FILE, 'r', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                resume.add(','.join(row))
-    return resume
-
-
-def save_resume(town, road, lane, alley, door):
-    """
-    將當前處理的位置保存到 resume.csv 文件中
+    從文件中讀取所有處理的位置
 
     Parameters
     ----------
-    town : str
-        鄉鎮名稱.
-    road : str
-        道路名稱.
-    lane : str
-        巷名稱.
-    alley : str
-        弄名稱.
-    door : str
-        號碼.
-        
+    file_name : str
+        檔案名稱.
+    start : int
+        欄位開始位置.
+    end : int
+        欄位結束位置.
+
     Returns
     -------
-    None
+    csv_file : set
+        包含已處理位置的集合.
 
     """
-    with open(RESUME_FILE, 'a', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow([town, road, lane, alley, door])
+    csv_file = set()
+    if os.path.exists(file_name):
+        with open(file_name, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                csv_file.add(','.join(row[start:end]))
+    return csv_file
+
+
+def svae_data(file_name, field_names, obj):
+    """
+    將當前處理的位置保存到文件中
+
+    Parameters
+    ----------
+    file_name : str
+        檔案名稱.
+    field_names : list
+        欄位名稱.
+    obj : dict
+        資料集.
+
+    Returns
+    -------
+    None.
+
+    """
+    with open(file_name, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        # 如果文件不存在，寫入表頭
+        if file.tell() == 0:
+            writer.writeheader()
+        writer.writerow(obj)
+        loguru.logger.debug(obj)
+
+
+def csv_to_json(file_name, output_name):
+    loguru.logger.info(f'資料集轉成JSON input:{file_name} ouput:{output_name}')
+    data = []
+    with open(file_name, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    with open(rf'data/{output_name}', 'w', encoding='utf-8') as json_file:
+        json.dump({"data": data}, json_file, ensure_ascii=False, indent=4)
 
 
 def process_towns(town):
@@ -364,20 +424,15 @@ def process_towns(town):
                     if position in processed_positions:
                         loguru.logger.info(f'{door} 已下載跳過')
                         continue
-                    
-                    dist = getXY(town, road, lane, alley, door)
-                    if dist:
-                        xy = Twd97ToWGS84(dist['X'], dist['Y'])
-                        addr = {'FULL_ADDR': dist['FULL_ADDR'], 'LATITUDE': round(xy[0], 5), 'LONGITUDE': round(xy[1], 5)}
-                        with open(CSV_FILE, 'a', newline='', encoding='utf-8') as file:
-                            writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
-                            # 如果文件不存在，寫入表頭
-                            if file.tell() == 0:
-                                writer.writeheader()
-                            writer.writerow(addr)
-                        loguru.logger.debug(addr)
-                    # 保存處理位置
-                    save_resume(town, road, lane, alley, door)
+
+                    obj = getXY(town, road, lane, alley, door)
+                    if obj:
+                        xy = Twd97ToWGS84(obj['X'], obj['Y'])
+                        # 要寫入的欄位名稱
+                        field_names = ['ADDR', 'LATITUDE', 'LONGITUDE', 'TOWN', 'ROAD', 'LANE', 'ALLEY', 'DOOR']
+                        my_dict = {'ADDR': obj['FULL_ADDR'], 'LATITUDE': xy[0], 'LONGITUDE': xy[1],
+                                   'TOWN': town, 'ROAD': road, 'LANE': lane, 'ALLEY': alley, 'DOOR': door}
+                        svae_data('address.csv', field_names, my_dict)
 
 
 if __name__ == '__main__':
@@ -385,10 +440,10 @@ if __name__ == '__main__':
     loguru.logger.info('Start get coordinates')
     session = requests.Session()
     # 讀取所有已處理的位置
-    processed_positions = read_resume()
+    processed_positions = read_data('address.csv', 3, 8)
     towns = getTowns()
     for i in range(0, len(towns), BATCH_SIZE):
-        batch_towns = towns[i:i+BATCH_SIZE]
+        batch_towns = towns[i:i + BATCH_SIZE]
         threads = []
         for town in batch_towns:
             thread = threading.Thread(target=process_towns, args=(town,))
@@ -397,4 +452,24 @@ if __name__ == '__main__':
 
         for thread in threads:
             thread.join()
+    csv_to_json('address.csv', 'address.json')
+    processed_positions = read_data('mark.csv', 3, 5)
+    mains = getMarkMainClassList()
+    for main in mains:
+        subs = getMarkSubClassList(main)
+        for sub in subs.keys():
+            # 如果已處理過此位置，則跳過
+            position = ','.join([mains[main], subs[sub]])
+            if position in processed_positions:
+                loguru.logger.info(f'main: {mains[main]} sub:{subs[sub]} 已下載跳過')
+                continue
+            mark_list = getMarkList(main, sub)
+            if mark_list:
+                for mark in mark_list:
+                    xy = Twd97ToWGS84(mark['X'], mark['Y'])
+                    field_names = ['NAME', 'LATITUDE', 'LONGITUDE', 'MAIN', 'SUB']
+                    my_dict = {'NAME': mark['LMarkName'], 'LATITUDE': xy[0], 'LONGITUDE': xy[1],
+                               'MAIN': mains[main], 'SUB': subs[sub]}
+                    svae_data('mark.csv', field_names, my_dict)
+    csv_to_json('mark.csv', 'mark.json')
     loguru.logger.info('Finish')
--- site.js
+++ site.js
@@ -72,43 +72,50 @@
             })
     })
 
-    let table = new DataTable('#my_table', {
-        responsive: true,
-        lengthChange: false,
-        rowReorder: false,
-        language: {
-            searchPlaceholder: "Search",
-            search: "",
-        },
-        ajax: {
-            url: 'data/data.json',
-            dataSrc: 'data'
-        },
-        columns: [
-            {
-                title: 'ADDR', data: 'ADDR',
-                render: function (data, type, row, meta) {
-                    if (type === 'display') {
-                        let params = {
-                            q: row['LATITUDE'] + ',' + row['LONGITUDE']
-                        };
-                        let url = "https://maps.google.com/?" + $.param(params, true);
-                        return '<a href="' + url + '" target="_blank">' + data + '</a>';
-                    }
-                    return data;
-                }
+    const buildTable = function (url, columns) {
+        let t = new DataTable('#my_table', {
+            stateSave: true,
+            responsive: true,
+            lengthChange: false,
+            rowReorder: false,
+            language: {
+                searchPlaceholder: "Search",
+                search: "",
             },
-            { title: 'LATITUDE', data: 'LATITUDE', visible: false },
-            { title: 'LONGITUDE', data: 'LONGITUDE', visible: false },
-            { title: 'TOWN', data: 'TOWN' },
-            { title: 'ROAD', data: 'ROAD' },
-            { title: 'LANE', data: 'LANE' },
-            { title: 'ALLEY', data: 'ALLEY' },
-            { title: 'DOOR', data: 'DOOR' }
-        ],
-        processing: true,
-        initComplete: function (settings, json) {
-            let info = settings.api.page.info();
+            ajax: {
+                url: url,
+                dataSrc: 'data'
+            },
+            columns: columns,
+            processing: true,
+            initComplete: function (settings, json) {
+                let info = settings.api.page.info();
+                let width = $(document).width();
+                if (width <= 767.98) {
+                    if (info.page == 0) {
+                        $('.swiper-button-prev').hide();
+                    } else {
+                        $('.swiper-button-prev').show();
+                    }
+                    if (info.page == info.pages - 1) {
+                        $('.swiper-button-next').hide();
+                    } else {
+                        $('.swiper-button-next').show();
+                    }
+                    $('#my_table_wrapper .dt-paging').hide();
+                } else {
+                    $('#my_table_wrapper .dt-paging').show();
+                }
+                $('#my_table_wrapper .dt-search').addClass('form-group has-search');
+                $(`<span class="form-control-feedback">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                </svg>
+                            </span>`).insertBefore('#dt-search-0');
+            }
+        });
+        t.on('page', () => {
+            let info = table.page.info();
             let width = $(document).width();
             if (width <= 767.98) {
                 if (info.page == 0) {
@@ -125,41 +132,58 @@
             } else {
                 $('#my_table_wrapper .dt-paging').show();
             }
-            $('#my_table_wrapper .dt-search').addClass('form-group has-search');
-            $(`<span class="form-control-feedback">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                        </svg>
-                    </span>`).insertBefore('#dt-search-0');
-        }
-    });
+        });
+        return t;
+    }
+    let addr_columns = [
+        {
+            title: 'ADDR', data: 'ADDR',
+            render: function (data, type, row, meta) {
+                if (type === 'display') {
+                    let params = {
+                        q: row['LATITUDE'] + ',' + row['LONGITUDE']
+                    };
+                    let url = "https://maps.google.com/?" + $.param(params, true);
+                    return '<a href="' + url + '" target="_blank">' + data + '</a>';
+                }
+                return data;
+            }
+        },
+        { title: 'LATITUDE', data: 'LATITUDE', visible: false },
+        { title: 'LONGITUDE', data: 'LONGITUDE', visible: false },
+        { title: 'TOWN', data: 'TOWN' },
+        { title: 'ROAD', data: 'ROAD' },
+        { title: 'LANE', data: 'LANE' },
+        { title: 'ALLEY', data: 'ALLEY' },
+        { title: 'DOOR', data: 'DOOR' }
+    ];
+    let mark_columns = [
+        {
+            title: 'NAME', data: 'NAME',
+            render: function (data, type, row, meta) {
+                if (type === 'display') {
+                    let params = {
+                        q: row['LATITUDE'] + ',' + row['LONGITUDE']
+                    };
+                    let url = "https://maps.google.com/?" + $.param(params, true);
+                    return '<a href="' + url + '" target="_blank">' + data + '</a>';
+                }
+                return data;
+            }
+        },
+        { title: 'LATITUDE', data: 'LATITUDE', visible: false },
+        { title: 'LONGITUDE', data: 'LONGITUDE', visible: false },
+        { title: 'MAIN', data: 'MAIN' },
+        { title: 'SUB', data: 'SUB' }
+    ];
 
+    let table = buildTable('data/data.json', addr_columns);
     $('.swiper-button-prev').on('click', () => {
         table.page('previous').draw(false);
     })
 
     $('.swiper-button-next').on('click', () => {
         table.page('next').draw(false);
-    });
-
-    table.on('page', () => {
-        let info = table.page.info();
-        let width = $(document).width();
-        if (width <= 767.98) {
-            if (info.page == 0) {
-                $('.swiper-button-prev').hide();
-            } else {
-                $('.swiper-button-prev').show();
-            }
-            if (info.page == info.pages - 1) {
-                $('.swiper-button-next').hide();
-            } else {
-                $('.swiper-button-next').show();
-            }
-            $('#my_table_wrapper .dt-paging').hide();
-        } else {
-            $('#my_table_wrapper .dt-paging').show();
-        }
     });
 
     $(window).resize(() => {
@@ -179,6 +203,23 @@
             $('#my_table_wrapper .dt-paging').hide();
         } else {
             $('#my_table_wrapper .dt-paging').show();
+        }
+    });
+
+    $('#navbarSupportedContent .nav-link').on('click', function () {
+        if ($(this).hasClass('active')) {
+            return;
+        }
+        let id = $(this).prop('id');
+        $('#navbarSupportedContent .nav-link').removeClass('active');
+        $(this).addClass('active');
+        table.destroy();
+        $('main').empty();
+        $('main').append(`<table class="table table-striped table-hover display" style="width: 100%;" id="my_table"></table>`);
+        if (id === 'btn_addr') {
+            table = buildTable('data/address.json', addr_columns);
+        } else {
+            table = buildTable('data/mark.json', mark_columns);
         }
     });
 })();
